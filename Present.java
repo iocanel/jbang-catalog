575c02819c9957c23d84656f31d2e330ff3f17f9
Ioannis Canellos <iocanel@gmail.com>
2023-05-12 16:01:26 +0300

    Add quarkus-present for serving markdown as Quarkus themed presentations

---
+++ Present.java
@@ -0,0 +1,121 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 17+ 
+//DEPS info.picocli:picocli:4.6.1
+//DEPS org.eclipse.jgit:org.eclipse.jgit:6.5.0.202303070854-r
+//DEPS org.slf4j:slf4j-simple:1.6.1
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
+
+import java.io.IOException;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.CopyOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Optional;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.*;
+
+@SuppressWarnings("deprecation")
+@Command(name = "present", mixinStandardHelpOptions = true, version = "1.0", description = "Serves reveal.js content via HTTP")
+class Present implements Runnable  {
+
+    private static final String JBANG_PRESENTATION_REPO = "https://github.com/iocanel/reveal.js.git";
+    private static final String JBANG_PRESENTATION_DIR = "JBANG_PRESENTATION_DIR";
+
+    @Parameters(index = "0", description = "The presentation file")
+    private File slidesFile;
+
+    @Option(names = {"-p", "--port"}, defaultValue = "8080", description = "The server port")
+    private int port;
+
+    @Option(names = {"-d", "--presentation-dir"}, description = "A local presentation directory to use")
+    private Optional<File> localPresentationDir = Optional.empty();
+
+    public static void main(String... args) {
+        CommandLine.run(new Present(), args);
+    }
+
+    @Override
+    public void run() {
+         try {
+            Path presentationDir = localPresentationDir.map(File::toPath)
+            .or(() -> getPresentationDirFromEnv()) 
+            .orElseGet(() -> clonePresentationRepo(JBANG_PRESENTATION_REPO));
+
+            Files.copy(slidesFile.toPath(), presentationDir.resolve(slidesFile.getName()), StandardCopyOption.REPLACE_EXISTING);
+
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/", exchange -> {
+                String requestPath = exchange.getRequestURI().getPath();
+                if (requestPath.equals("/")) {
+                   requestPath = "/index.html";
+                }
+                File requestedFile = new File(presentationDir.toFile(), requestPath.substring(1));
+                if (requestedFile.exists() && requestedFile.isFile()) {
+                    byte[] fileBytes = Files.readAllBytes(requestedFile.toPath());
+                    exchange.sendResponseHeaders(200, fileBytes.length);
+                    exchange.getResponseBody().write(fileBytes);
+                    exchange.getResponseBody().close();
+                } else {
+                    String response = "File not found: " + requestPath;
+                    exchange.sendResponseHeaders(404, response.length());
+                    exchange.getResponseBody().write(response.getBytes());
+                    exchange.getResponseBody().close();
+                }
+            });
+            server.start();
+            System.out.println("Server started on port " + port);
+        } catch (IOException e) {
+            System.err.println("Error starting the server: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Get the {@link Path} of that 'JBANG_PRESENTATION_DIR` points to.
+     * @return the {@link Path} wrapped in {@link @Optional}.
+     **/
+    private static Optional<Path> getPresentationDirFromEnv() {
+        return Optional.ofNullable(System.getenv(JBANG_PRESENTATION_DIR)).map(d -> Paths.get(d));
+    }
+
+    /**
+     * Clone the reveal.js repository to temporary directory
+     * @param repositoryUrl The url of the repository to clone.
+     * @return the {@link Path} to the directory
+     **/
+    private static Path clonePresentationRepo(String repositoryUrl) {
+           System.out.println("Cloning reveal.js repository:" + repositoryUrl);
+           try {
+               Path cloneDir = Files.createTempDirectory("presentation-jbang");
+                // Clone the repository
+                CloneCommand cloneCommand = Git.cloneRepository()
+                .setURI(repositoryUrl)
+                .setDirectory(cloneDir.toFile());
+                Git git = cloneCommand.call();
+                Repository repository = git.getRepository();
+
+                // Clear the Git configuration
+                File dotGit = repository.getDirectory();
+                File config = new File(dotGit, Constants.CONFIG);
+                config.delete();
+
+                return dotGit.getParentFile().toPath();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+    }
+}
+
